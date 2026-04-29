36750d71322b8fe13dd2cb6256067cdc10dad9f9
Jack Stanek <stanekjack@gmail.com>
2026-04-28 21:03:52 -0500

    feat: add logging functionality

--- puzzle-gen/src/main.rs
+++ puzzle-gen/src/main.rs
@@ -1,5 +1,6 @@
+use anyhow::Context;
 use clap::Parser;
-use log::{debug, info};
+use log::{Level, debug, info};
 
 #[macro_use]
 mod macros;
@@ -9,9 +10,28 @@ mod theories;
 
 #[derive(Parser, Debug)]
 #[command(version)]
-pub struct Args {}
+pub struct Args {
+    /// Log level
+    #[arg(long, default_value_t = Level::Info)]
+    log_level: Level,
+}
 
-fn main() {
+/// Set up the logger.
+///
+/// Log all messages above the specified log level.
+fn logger_setup(level: &Level) -> anyhow::Result<()> {
+    env_logger::builder()
+        .filter_level(level.to_level_filter())
+        .try_init()
+        .context("couldn't initialize logger")
+}
+
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    logger_setup(&args.log_level)?;
+    info!("puzzle-gen started");
     debug!("{:?}", &args);
+
+    info!("exiting");
+    Ok(())
 }
